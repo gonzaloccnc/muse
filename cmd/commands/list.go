8f20ae67e968a6fb30fa2040f27abf0e149f42c3
gonzaloccnc <gonzadevelw@mail.com>
2024-03-15 23:00:31 -0500

    feat: add support for list command

--- cmd/commands/list.go
+++ cmd/commands/list.go
@@ -3,19 +3,26 @@ package commands
 import (
 	"fmt"
 	ck "muse/cmd/commands/make"
+	"muse/db"
+	"strings"
 
 	"github.com/charmbracelet/lipgloss"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
-var templateStyle = lipgloss.NewStyle().
-	Bold(true).
-	Margin(1).
-	Foreground(lipgloss.Color("#8934eb")).
-	BorderLeft(true).
-	BorderForeground(lipgloss.Color("#8934eb")).
-	BorderStyle(lipgloss.NormalBorder())
+var (
+	templateStyle = lipgloss.NewStyle().
+			Bold(true).
+			Margin(1).
+			Foreground(lipgloss.Color("#8934eb")).
+			BorderLeft(true).
+			BorderForeground(lipgloss.Color("#8934eb")).
+			BorderStyle(lipgloss.NormalBorder())
+
+	purpleStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#8934eb"))
+	itemStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("#16cc29"))
+)
 
 var ListCmd = &cobra.Command{
 	Use:   "list [language_abbr]",
@@ -27,7 +34,14 @@ var ListCmd = &cobra.Command{
 			return
 		}
 
-		getViteTemplates()
+		// get from a map
+		if args[0] == "alias" {
+			listAliases()
+		}
+
+		if args[0] == "ts" || args[0] == "js" {
+			getViteTemplates()
+		}
 	},
 }
 
@@ -44,5 +58,30 @@ func getViteTemplates() {
 }
 
 func GetBackJsTemplates() {
+
+}
+
+func listAliases() {
+	aliases := db.GetJsonFile(db.JsonFile)
+
+	s7 := strings.Repeat("-", 7)
+	s12 := strings.Repeat("-", 12)
+	s22 := strings.Repeat("-", 22)
+
+	borderStyle := fmt.Sprintf("|%s|%s|%s|", s7, s12, s22)
+	slashStyle := purpleStyle.Render("|")
+
+	title := fmt.Sprintf("| %-5s | %-10s | %-20s |", "Order", "Alias", "Path")
+	title = strings.ReplaceAll(title, "|", slashStyle)
+
+	fmt.Println(purpleStyle.Render(borderStyle))
+	fmt.Println(title)
+	fmt.Println(purpleStyle.Render(borderStyle))
+
+	for i, val := range aliases.Aliases {
+		item := fmt.Sprintf("| %-5d | %-10s | %-20s |", i+1, val.Alias, val.Path)
+		fmt.Println(itemStyle.Render(item))
+		fmt.Println(purpleStyle.Render(borderStyle))
+	}
 
 }
